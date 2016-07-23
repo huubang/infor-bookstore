c1cc5f472265a89293c0b2f1c4eb96d200888345
huubang <codeforfood@outlook.com>
2016-07-23 22:32:17 +1000

    Add Data project

---
+++ Infor.BookStore.Data/Models/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Infor.BookStore.Data.Models
+{
+    public partial class Book
+    {
+        [ImportField("Name")]
+        public string Name { get; set; }
+
+        [ImportField("Isbn")]
+        public string Isbn { get; set; }
+
+        [ImportField("Author")]
+        public string Author { get; set; }
+    }
+
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ImportFieldAttribute : Attribute
+    {
+        public string Name { get; set; }
+
+        public ImportFieldAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
